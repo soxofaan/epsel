d5ae26ae3a31e4eb12bb3a3ab33e5cbfa5db4208
Stefaan Lippens <soxofaan@gmail.com>
2020-04-09 09:42:25 +0200

    Bump to version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="epsel",
-    version="0.1.0",
+    version="1.0.0",
     author="Stefaan Lippens",
     description="Ensure PySpark Executor Logging",
     long_description=long_description,
